3a465df524560844ad8be30e76a72348a48b6e71
Gilbert kitetu <gilbertkitetu2@gmail.com>
2021-09-25 13:49:24 +0300

    deploy

--- server/index.js
+++ server/index.js
@@ -2,14 +2,22 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const path = require('path');
 const { Server } = require("socket.io");
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+  
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
